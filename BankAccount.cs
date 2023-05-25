f555ec71a197131dfd402ecdcc3b448dbfb0f44a
XavielCesarJoao <xavielcesar.joao@gmail.com>
2023-05-25 21:10:21 +0100

    Primeiro Commit do dia

---
+++ BankAccount.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_List
+{
+    class BankAccount
+    {
+        private static int accountNumberSeed = 1234567890;
+        public string Number { get; }
+        public string Owner { get; set; }
+        public string Note { get; set; }
+        public decimal Balance 
+        {   get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransations)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        public BankAccount (string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+            MakeDeposit(initialBalance, DateTime.Now , "Abertura de conta para " + Owner);
+        }
+        private List<Transaction> allTransations = new List<Transaction>();
+        public void MakeDeposit (decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "O valor do depósito deve ser positivo");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransations.Add(deposit);
+        }
+        public void MakeWithdrawal (decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "O valor para retirada deve ser positivo");
+            }
+            if(Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Não há fundos suficientes para esta retirada");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransations.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach(var item in allTransations)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankAccount accountAna = new BankAccount("Ana Maria",  20000);
+            accountAna.MakeDeposit(40000, DateTime.Now, "Deposito");
+            Console.WriteLine(accountAna.GetAccountHistory());
+            //BankAccount accountJorge = new BankAccount("Jorge Renato Bartolomeu", 50000 );
+            // IMPRIMINDO AS CONTAS
+
+            //  Console.WriteLine(accountJorge.GetAccountHistory());
+            //Console.WriteLine(accountJorge.Balance);
+
+            BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("Invalid", -66);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exceção capturada criando conta com saldo negativo");
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
